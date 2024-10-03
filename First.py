59193b52be471e3d2681b1e29210bf874c93e772
Azamat <abdullayevazamat2705@gmail.com>
2024-10-03 23:24:54 +0500

    Backend sonni tublikka tekshiradigan dastur yozdi

--- First.py
+++ First.py
@@ -5,3 +5,17 @@ def sonalar(son: int) -> int:
     return son//2
 
 print(sonalar(16))
+
+
+def tub(son: int) -> bool:
+    a = 0
+    for i in range(1, son+1):
+        if son % i == 0:
+            a += 1
+
+    if a == 2:
+        return True
+    else: return False
+
+
+print(tub(13))
